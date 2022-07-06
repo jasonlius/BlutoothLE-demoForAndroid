68ae78ebcc750f994bfeb61fc6af2d3415b8bd9f
mac <2582909040@qq.com>
2022-07-06 15:29:19 +0800

    frist commmit

---
+++ app/src/main/java/com/example/blegattproject/Constants.java
@@ -0,0 +1,8 @@
+package com.example.blegattproject;
+
+public class Constants {
+    public static final String TAG = "bdsk";
+    public static final String FIND = "寻找蓝牙设备";
+    public static final String STOP_SCANNING = "停止扫描中...";
+    public static final String SCANNING = "扫描中...";
+}
